580ef0ae76fa8c65ae133568de5cce743521bfdc
Afiq Fahmi <afqifahmi9858@gmail.com>
2024-03-13 23:50:41 +0800

    Update beginner_page.dart

--- lib/pages/beginner_page.dart
+++ lib/pages/beginner_page.dart
@@ -6,17 +6,177 @@ class BeginnerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Another Page'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Center(
-        child: Text('This is another page.'),
+      body: Stack(
+        children: [
+          Container(
+            height: 300,
+            padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(70),
+                bottomRight: Radius.circular(60),
+              ),
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF00002136),
+                  Color(0xFF104A57),
+                ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.center,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 30,
+            left: 10,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.white,
+            ),
+          ),
+          const Positioned(
+            top: 40,
+            left: 145,
+            child: Text(
+              "Let's Learn!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 100, left: 25, right: 25, bottom: 30),
+              child: Stack(
+                // Wrap your content with Stack
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 10),
+                          Text(
+                            "Investing ",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Module 1  - 30 Minutes  ",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            "Investing is when you buy something with the expectation that it will earn you money over time. Whether your goal is to buy a home, pay for college, or retire, there is an investment option that will meet your needs.",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w200,
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "What’s Included ",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 10.0),
+                                child: Icon(Icons.videocam),
+                              ),
+                              Text("2 Videos "),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 10.0),
+                                child: Icon(Icons.class_),
+                              ),
+                              Text("4 Flashcards"),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 10.0),
+                                child: Icon(Icons.quiz),
+                              ),
+                              Text("1 Quiz"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: Container(
+                      width: 120,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x3f000000),
+                            offset: Offset(0, 4),
+                            blurRadius: 2,
+                          ),
+                          BoxShadow(
+                            color: Color(0x3f000000),
+                            offset: Offset(0, 4),
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Enroll Now",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
