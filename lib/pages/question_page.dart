46261401a27f190df3b6d965aeda946cff581fc6
Afiq Fahmi <afqifahmi9858@gmail.com>
2024-03-19 19:02:21 +0800

    Update question_page.dart

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -27,26 +27,9 @@ class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF3C3279),
       body: Stack(
         children: [
-          Container(
-            height: 300,
-            padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(70),
-                bottomRight: Radius.circular(60),
-              ),
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xFF00002136),
-                  Color(0xFF104A57),
-                ],
-                begin: Alignment.bottomCenter,
-                end: Alignment.center,
-              ),
-            ),
-          ),
           Positioned(
             top: 30,
             left: 10,
@@ -75,157 +58,155 @@ class _QuestionPageState extends State<QuestionPage> {
                   top: 100, left: 25, right: 25, bottom: 30),
               child: Stack(
                 children: [
-                  Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 3,
-                          blurRadius: 10,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: PageView.builder(
-                        controller: _controller!,
-                        onPageChanged: (page) {
-                          if (page == questions.length - 1) {
-                            setState(() {
-                              btnText = "See Results";
-                            });
-                          }
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: PageView.builder(
+                      controller: _controller!,
+                      onPageChanged: (page) {
+                        if (page == questions.length - 1) {
                           setState(() {
-                            answered = false;
+                            btnText = "See Results";
                           });
-                        },
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
+                        }
+                        setState(() {
+                          answered = false;
+                        });
+                      },
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Question ${index + 1}/10",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Flexible(
+                              child: Container(
+                                padding: EdgeInsets.all(12),
+                                height: double.infinity,
                                 width: double.infinity,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      "Question ${index + 1}/10",
-                                      textAlign: TextAlign.start,
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 15.0,
-                                      ),
-                                    ),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.05),
+                                      blurRadius: 15.0,
+                                      offset: Offset(0, 7),
+                                    )
                                   ],
                                 ),
-                              ),
-                              SizedBox(
-                                height: 10.0,
-                              ),
-                              Flexible(
                                 child: Text(
                                   "${questions[index].question}",
-                                  style: TextStyle(
+                                  style: const TextStyle(
                                     color: Colors.black,
                                     fontSize: 22.0,
+                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
                               ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              for (int i = 0;
-                                  i < questions[index].answers!.length;
-                                  i++)
-                                Container(
-                                  width: double.infinity,
-                                  height: 80.0,
-                                  margin: EdgeInsets.only(
-                                      bottom: 20.0, left: 12.0, right: 12.0),
-                                  child: RawMaterialButton(
-                                    padding: EdgeInsets.all(10),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                    ),
-                                    fillColor: btnPressed
-                                        ? questions[index]
-                                                .answers!
-                                                .values
-                                                .toList()[i]
-                                            ? Colors.green
-                                            : Colors.red
-                                        : Colors.blue,
-                                    onPressed: !answered
-                                        ? () {
-                                            if (questions[index]
-                                                .answers!
-                                                .values
-                                                .toList()[i]) {
-                                              score++;
-                                              print("yes");
-                                            } else {
-                                              print("no");
-                                            }
-                                            setState(() {
-                                              btnPressed = true;
-                                              answered = true;
-                                            });
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            for (int i = 0;
+                                i < questions[index].answers!.length;
+                                i++)
+                              Container(
+                                width: double.infinity,
+                                height: 80.0,
+                                margin: EdgeInsets.only(
+                                    bottom: 20.0, left: 12.0, right: 12.0),
+                                child: RawMaterialButton(
+                                  padding: EdgeInsets.all(10),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  fillColor: btnPressed
+                                      ? questions[index]
+                                              .answers!
+                                              .values
+                                              .toList()[i]
+                                          ? Colors.green
+                                          : Colors.red
+                                      : Color(0xFF6A5ACD),
+                                  onPressed: !answered
+                                      ? () {
+                                          if (questions[index]
+                                              .answers!
+                                              .values
+                                              .toList()[i]) {
+                                            score++;
+                                            print("yes");
+                                          } else {
+                                            print("no");
                                           }
-                                        : null,
-                                    child: Text(
-                                        questions[index]
-                                            .answers!
-                                            .keys
-                                            .toList()[i],
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 15.0,
-                                        )),
-                                  ),
+                                          setState(() {
+                                            btnPressed = true;
+                                            answered = true;
+                                          });
+                                        }
+                                      : null,
+                                  child: Text(
+                                      questions[index]
+                                          .answers!
+                                          .keys
+                                          .toList()[i],
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      )),
                                 ),
-                              SizedBox(
-                                height: 10.0,
                               ),
-                              RawMaterialButton(
-                                onPressed: () {
-                                  if (_controller!.page?.toInt() ==
-                                      questions.length - 1) {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                ResultPage(score)));
-                                  } else {
-                                    _controller!.nextPage(
-                                        duration: Duration(milliseconds: 250),
-                                        curve: Curves.easeInExpo);
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            RawMaterialButton(
+                              onPressed: () {
+                                if (_controller!.page?.toInt() ==
+                                    questions.length - 1) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ResultPage(score)));
+                                } else {
+                                  _controller!.nextPage(
+                                      duration: Duration(milliseconds: 250),
+                                      curve: Curves.easeInExpo);
 
-                                    setState(() {
-                                      btnPressed = false;
-                                    });
-                                  }
-                                },
-                                shape: StadiumBorder(),
-                                fillColor: Colors.blue,
-                                padding: EdgeInsets.all(18.0),
-                                elevation: 0.0,
-                                child: Text(
-                                  btnText,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              )
-                            ],
-                          );
-                        },
-                        itemCount: questions.length,
-                      ),
+                                  setState(() {
+                                    btnPressed = false;
+                                  });
+                                }
+                              },
+                              shape: StadiumBorder(),
+                              fillColor: Colors.blue,
+                              padding: EdgeInsets.all(18.0),
+                              elevation: 0.0,
+                              child: Text(
+                                btnText,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                      itemCount: questions.length,
                     ),
                   ),
                 ],
